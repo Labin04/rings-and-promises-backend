48a0016faad7dd40a9d52579fa3f3edf474ce457
dharma2532@gmail.com <dharma2532@gmail.com>
2026-08-24 11:47:14 +0530

    restrutured

--- rings-and-promises/src/main/java/com/ringsandpromises/backend/contact/ContactInquiryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ringsandpromises.backend.repository;
-
-import com.ringsandpromises.backend.entity.ContactInquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactInquiryRepository extends JpaRepository<ContactInquiry, Long> {
-}
